3355dfdeeaad8671ee78c5f1d042805d55d0a128
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-11 16:29:48 +0200

    switched to Fenrir's match history, databases for events now in guild specific folders, added event removal function added logging, switched to pathlib

--- functions.py
+++ functions.py
@@ -3,19 +3,21 @@ import requests
 import pandas as pd
 import datetime as dt
 import shelve
-import os
+import logging
+import requests
 
 import cfg
 import utils
 
 
+
 def match_history(home_team = "Fenrir"):        # TODO: better formatting of output
     'Returns string of previous matches. Output in monospace characters recommended'
     
-    url = r"https://vrmasterleague.com/EchoArena/Teams/I0s62s81gK1eswlVkTNz6Q2"
+    url = r"https://vrmasterleague.com/EchoArena/Teams/SRo_nCsh6RT2Py5X5_iUyw2"
     r = requests.get(url)
     tables = pd.read_html(r.text)
-    matches = tables[2]
+    matches = tables[-1]
     
     matches = matches.drop(columns=["VOD", "MATCH PAGE"])
     matches["DATE PLAYED"] = pd.to_datetime(matches["DATE PLAYED"]).dt.date
@@ -39,34 +41,36 @@ def match_history(home_team = "Fenrir"):        # TODO: better formatting of out
     
     return "\n".join(lines)
 
-    return str(clean_matches)
 
 
 # maybe move over to utils
 def get_events(guild):
     'Return dict of events for guild'
-    with shelve.open(os.path.join(cfg.SCRIPT_DIR, "data", "database")) as db:
-        if not str(guild.id) in db:
-            db[str(guild.id)] = {}
-        try:
-            events = db[str(guild.id)]["events"]
-        except KeyError:
-            events = {}
-    return events
+    with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "events")) as events:
+        out = {dt.datetime.fromisoformat(k):v for k,v in events.items()}
+        return out
+
 
 # maybe move over to utils
-def add_event(guild, date, name):
-    with shelve.open(os.path.join(cfg.SCRIPT_DIR, "data", "database"), writeback=True) as db:
-        if not str(guild.id) in db:
-            db[str(guild.id)] = {}
-        if not "events" in db[str(guild.id)]:
-            db[str(guild.id)]["events"] = {date: name}
-        
-        db[str(guild.id)]["events"][date] = name
-    
+def add_event(guild: discord.guild, date: dt.datetime, name: str):
+    with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "events")) as events:
+        events[date.isoformat()] = name
     return True
 
 
+def rem_event(guild, date: dt.datetime):
+    logging.debug(f"Removing event {date} from event db for guild {guild.name}, ID: {guild.id}.")
+    with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "events")) as events:
+        try:
+            event = events.pop(date.isoformat())
+        except KeyError:
+            logging.error(f"No event with {date} exists in the event planner.")
+            return False
+    return True, event
+
+
+
+
 if __name__ == "__main__":
-    # print(match_history("Team Gravity"))
+    print(match_history())
     pass
