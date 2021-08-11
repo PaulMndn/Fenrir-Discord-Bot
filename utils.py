ca24446aecc280cda61b9a4541f53717cc19d54b
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-11 16:40:13 +0200

    added logging, switched to pathlib, clean-up of unused functions, added ensure_guild_folder, get_default_settings, get_guild_settings

--- utils.py
+++ utils.py
@@ -1,35 +1,24 @@
 import os
 import json
 import shelve
+import logging
 
 import cfg
 
 
 
-def read_json(fp):
-    'return data from json'
-    with open(fp, "r") as f:
-        data = json.load(f)
-    return data
-
 def get_config():
     'import config.json'
-    cf = os.path.join(cfg.SCRIPT_DIR + "config.json")
-    if not os.path.exists(cf):
-        print("Config doesn't exist!")
+    logging.info("Import config.json")
+    cf = cfg.SCRIPT_DIR / "config.json"
+    if not cf.exists():
+        logging.critical("Config file does not exist! Programm will shut down.")
         import sys
         sys.exit()
-    return read_json(cf)
+    
+    with open(cf, "r") as config:
+        return json.load(config)
 
-
-def write_db(guild, key, data):
-    fp = os.path.join(cfg.SCRIPT_DIR, "data", "database")
-    with shelve.open(fp, writeback=True) as db:
-        if not str(guild.id) in db:
-            db[str(guild.id)] = {}
-        
-        db[str(guild.id)][key] = data
-    return True
 
 def read_db(guild, key):
     fp = os.path.join(cfg.SCRIPT_DIR, "data", "database")
@@ -45,8 +34,34 @@ def read_db(guild, key):
     else:
         return data
 
-def get_planner():
-    pass
 
-def save_planner():
-    pass
+def ensure_guild_folder(guild):
+    "Make sure guild data folder exists."
+    dir = cfg.DATA_DIR / str(guild.id)
+    if not dir.exists:
+        dir.mkdir()
+
+
+def get_default_settings():
+    'get default settings from data/default.json'
+    fp = str(cfg.DATA_DIR/"default_settings.json")
+    return json.load(fp)
+
+
+def get_guild_settings(guild):
+    '''Get guild settings.
+    
+    Args:
+        guild: guild object
+    
+    Returns:
+        dict of guild settings
+    '''
+    guild_settings = get_default_settings()
+
+    # add/overwrite with guild-specific settings
+    with shelve.open(str(cfg.DATA_DIR/str(guild.id)/"settings")) as settings:
+        for k,v in settings.items():
+            guild_settings[k] = v
+    
+    return guild_settings
