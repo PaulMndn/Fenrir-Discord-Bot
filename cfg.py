30e09ca0338f965318e5b7166bf93bd818cfb324
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-11 16:14:02 +0200

    switched from os.path to pathlib, switched placeholder in join/leave msgs to capital letters

--- cfg.py
+++ cfg.py
@@ -1,11 +1,11 @@
-import os
+# import os
+import pathlib
 import json
 import utils
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-SCRIPT_DIR = SCRIPT_DIR + ("/" if not SCRIPT_DIR.endswith("/") else "")
-
-
+SCRIPT_DIR = pathlib.Path()
+DATA_DIR = SCRIPT_DIR / "data"
+# SCRIPT_DIR = SCRIPT_DIR + ("/" if not SCRIPT_DIR.endswith("/") else "")
 
 CONFIG = utils.get_config()
 
@@ -14,21 +14,20 @@ BOT_TEST_CHANNEL_ID = CONFIG["bot_test-channel"]
 PREFIX = "$"
 
 
-# "member" will be replaced by the mention-tag
+# "MEMBER" will be replaced by the mention-tag
 JOIN_MSGS = [
-    f"Welcome member! You can't leave now."
-    f"Wrong Server member!",
-    f"This is not the afterlife member!",
-    f"member, Turn around!"
+    "Welcome MEMBER! You can't leave now.",
+    "Wrong Server MEMBER!",
+    "This is not the afterlife MEMBER!",
+    "MEMBER, Turn around!"
 ]
 
-# "member" will be replaced by the mention tag
+# "MEMBER" will be replaced by name#discriminator
 LEAVE_MSGS = [
-    f"Ok, bye then, member..."
-    f"You're a fish, member.",
-    f"It was all a dream, member.",
-    f"member, nothing is true.",
-    f"member, I have nothing."
+    "Ok, bye then, MEMBER...",
+    "You're a fish, MEMBER.",
+    "It was all a dream, MEMBER.",
+    "MEMBER, nothing is true.",
+    "MEMBER, I have nothing."
 ]
 
-# PLANNER = load_planner()
