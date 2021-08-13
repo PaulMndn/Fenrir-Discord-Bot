aa5905bd0099d04dcc9af976630b2927625ab76c
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-13 23:47:14 +0200

    outsourced shelve handeling, include reset option, reworked modifying settings, included validity checks

--- commands/settings.py
+++ commands/settings.py
@@ -1,8 +1,10 @@
+import discord
 import shelve
 import logging
 
 from commands.base import Cmd
 import cfg
+from utils import get_guild_settings, reset_guild_settings
 
 help_text = """Display and edit the bot settings for this server.
 
@@ -26,28 +28,59 @@ EXAMPLES:
 
 async def execute(ctx, params):
     logging.info(f"Executing command {ctx['command']} {ctx['params_str']}.")
-    guild = ctx['guild']
+    guild:discord.Guild = ctx['guild']
     
     if len(params) == 0:
         # no further parameters, return all current values
-        with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "settings.json")) as settings:
-            ret = "```\n" + "\n".join(f"{s<20}{v}" for s,v in settings.items()) + "\n```"
-            logging.debug("Returning all settings with current values")
-            return True, ret
+        settings = get_guild_settings(guild)
+        ret = "Your current settings:\n```\n" + "\n".join(f"{s:<20}{v}" for s,v in settings.items()) + "\n```"
+        logging.debug("Returning all settings with current values")
+        return True, ret
+    
+    elif len(params) == 1 and params[0] == "reset":
+        old_settings = get_guild_settings(guild)
+        old_settings = "\n".join(f"{k:<20}{v}" for k,v in old_settings.items())
+        reset_guild_settings(guild)
+        return True, "Your settings have been reset to the default values.\n\n" \
+            f"These were your settings before:\n```\n{old_settings}\n```"
 
     elif len(params) == 2:
         # setting and new value included
         setting = params[0]
         new_val = params[1]
-        with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "settings.json")) as settings:
-            if setting not in settings:
-                return False, f"Setting {setting} does not exist."
-            old_val = settings[setting]
+        guild_settings = get_guild_settings(guild)
+        
+        if setting not in guild_settings:
+            return False, f"Setting {setting} does not exist."
+
+        # check validity of new value
+        id = int(new_val.strip("<>#@!&"))
+        if setting == "events_channel" and guild.get_channel(id) is None:
+            logging.error(f"Invalid channel to set as events_channel for guild {guild.name}, ID: {guild.id}")
+            return False, "Invalide channel"
+        elif setting == "team_role" and guild.get_role(id) is None:
+            logging.error(f"Invalid role to set as team_role for guild {guild.name}, ID: {guild.id}")
+            return False, "Invalide Channel"
+        
+        # update settings
+        old_val = guild_settings[setting]
+        with shelve.open(str(cfg.DATA_DIR / str(guild.id) / "settings")) as settings:
             settings[setting] = new_val
             logging.info(f"Changed setting {setting} from {old_val} to {new_val} for server {ctx['guild'].name}, ID: {ctx['guild'].id}.")
-            return True, f"{setting} was changed from {old_val} to {new_val}."
+        
+        return True, f"{setting} was changed from {old_val} to {new_val}."
 
     else:
         # Invalid number of parameters
-        return False, "2 Parameters required to modify a setting: the setting and the new value."
+        return False, "Invalid parameters. See help for more information."
     
+
+
+
+
+command = Cmd(
+    execute=execute,
+    help_text=help_text,
+    params_required=0,
+    admin_required=True
+)
