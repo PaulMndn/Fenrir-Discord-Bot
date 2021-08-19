67f10336d57e3d825fedeb280f9c5fe3203f2782
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-19 19:39:37 +0200

    new Event class

---
+++ lib/event.py
@@ -0,0 +1,64 @@
+import discord
+import datetime as dt
+
+
+class Event:
+    def __init__(self, title: str, date_time: dt.datetime, event_channel_id: int, msg_id: int):
+        self.title = title
+        self.date_time = date_time
+        self.event_channel_id = event_channel_id
+        self.msg_id = msg_id
+    
+    def __str__(self):
+        return f"**{self.title}** {self.date_time.strftime('%d.%m.%Y at %I:%M%p')}."
+    
+    def __lt__(self, other):
+        if isinstance(other, Event):
+            return self.date_time < other.date_time
+        elif isinstance(other, dt.datetime):
+            return self.date_time < other
+        elif isinstance(other, dt.date):
+            return self.date_time.date() < other
+        else:
+            raise NotImplementedError(f"Can't compare type '{type(self)}'' to type '{type(other)}'.")
+
+    def __le__(self, other):
+        if isinstance(other, Event):
+            return self.date_time <= other.date_time
+        elif isinstance(other, dt.datetime):
+            return self.date_time <= other
+        elif isinstance(other, dt.date):
+            return self.date_time.date() <= other
+        else:
+            raise NotImplementedError(f"Can't compare type '{type(self)}'' to type '{type(other)}'.")
+    
+    def __ge__(self, other):
+        if isinstance(other, Event):
+            return self.date_time >= other.date_time
+        elif isinstance(other, dt.datetime):
+            return self.date_time >= other
+        elif isinstance(other, dt.date):
+            return self.date_time.date() >= other
+        else:
+            raise NotImplementedError(f"Can't compare type '{type(self)}'' to type '{type(other)}'.")
+    
+    def __eq__(self, other):
+        if isinstance(other, Event):
+            return self.date_time == other.date_time
+        elif isinstance(other, dt.datetime):
+            return self.date_time == other
+        elif isinstance(other, dt.date):
+            return self.date_time.date() == other
+        else:
+            raise NotImplementedError(f"Can't compare type '{type(self)}'' to type '{type(other)}'.")
+    
+    @property
+    def key(self):
+        return None if self.msg_id is None else str(self.msg_id)
+
+
+
+
+
+if __name__ == "__main__":
+    pass
