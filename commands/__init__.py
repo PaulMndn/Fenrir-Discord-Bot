0207d3912c97b8bb8f012588a277bc76f98402e7
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-08-27 17:42:46 +0200

    added potato cmd, admin can execute team commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -8,18 +8,20 @@ from . import (
     help,
     history,
     ping,
+    potato,
     settings,
 )
 
 
 
 commands = {
-    "availibility": availability.command,
+    "availability": availability.command,
     "add-event": event_add.command,
     "events": events.command,
     "help": help.command,
     "history": history.command,
     "ping": ping.command,
+    "potato": potato.command,
     "remove-event": event_remove.command,
     "settings": settings.command
 }
@@ -32,6 +34,9 @@ async def run(c, params, ctx):
     cmd = commands[c]
 
     if cmd.admin_required and not ctx["admin"]:
+        return False, "Sorry. You don't have the required permissions for this command."
+    
+    if cmd.team_required and not (ctx['team'] or ctx['admin']):
         return False, "Sorry. You don't have the required permissions for this command."
     
     if len(params) < cmd.params_required:
